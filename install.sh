769af08f7ad10df7e2d1acc7df1dc1afcd7cfa0f
xss <58095671+CipherVector@users.noreply.github.com>
2021-08-23 14:19:09 +0800

    Fixed some path issues

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-sudo pacman -S yay
+sudo pacman --noconfirm -Syu
+sudo pacman --noconfirm  -S yay xorg xorg-xinit xorg-xinput
 
 yay -S alacritty bspwm dunst eww i3 kitty mpd ncmpcpp neovim picom polybar qutebrowser ranger rofi zathura zsh 
 git clone https://github.com/mlvzk/discocss
@@ -14,19 +15,19 @@ mv ~/.xprofile ~/.xprofile-bak
 mv ~/.zshrc ~/.zshrc-bak
 mv ~/Pictures ~/Pictures-bak
 mv ~/.scripts ~/.scripts-bak
-rm -rf .config
-cp -r /config ~/.config
-cp /config/.Xauthority ~/
-cp /config/.Xresources ~/
-cp /config/.fehbg ~/
-cp /config/.p10k.zsh ~/
-cp /config/.tmux.conf ~/
-cp /config/.xprofile ~/
-cp /config/.zshrc ~/
-cp -r /config/.script ~/
-cp -r /config/.ascii ~/
-cp -r /config/.local/bin ~/
-cp -r /Pictures ~/
-cp -r /.scripts ~/
-cp -r /fonts/* /usr/share/fonts/
+rm -rf ~/.config
+cp -r ./config ~/.config
+cp ./config/.Xauthority ~/
+cp ./config/.Xresources ~/
+cp ./config/.fehbg ~/
+cp ./config/.p10k.zsh ~/
+cp ./config/.tmux.conf ~/
+cp ./config/.xprofile ~/
+cp ./config/.zshrc ~/
+cp -r ./config/.script ~/
+cp -r ./config/.ascii ~/
+cp -r ./config/.local/bin ~/
+cp -r ./Pictures ~/
+cp -r ./.scripts ~/
+cp -r ./fonts/* /usr/share/fonts/
 mkdir ~/screenshots
