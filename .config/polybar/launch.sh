c58a8067c7a76e493367e559eb5e988184554dbe
Detective Pikachu <frenzyexists@gmail.com>
2021-11-06 10:32:18 -0400

    Blasphemy

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/zsh
+
+sleep .5
+
+if ! pgrep -x polybar; then
+			
+	polybar -c ~/.config/polybar/config top &
+else
+	# Kill prev polybar session
+	pkill -USR1 polybar
+fi
+
+echo "Bar launched..."
